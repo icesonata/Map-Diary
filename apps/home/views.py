33a5f5ebc93a4d3d74229e27414b0726b88365bd
Long Nguyen <hoanglong1622000@gmail.com>
2021-09-17 03:07:34 +0700

    Removed redundant lines of code

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
+from apps.user.models import UserProfile
 
 # Create your views here.
 def index(request):
-    # if request.user.is_authenticated:
-    #     print(request.user.profile.profile_url)
-    username = request.user.username
-    context = {"username": username}
-    return render(request, "home/home.html", context)
+    return render(request, "home/home.html")
